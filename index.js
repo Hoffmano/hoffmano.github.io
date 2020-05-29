4da867cb219b461c46d549e498e889954ac95228
Gabriel Hoffman Silva <gabrielhoffman@usp.br>
2020-05-29 17:31:18 -0300

    mensagem de habitos concluidos com sucesso

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 document.addEventListener('click', function (event) {
-    console.log(event.target);
 
     if (event.target.matches('.list-group-item-action')) {
+        const parentNode = event.target.parentElement;
         event.target.remove();
+        console.log(parentNode);
+        console.log(parentNode.childElementCount);
+        if (parentNode.childElementCount == 0) {
+            parentNode.insertAdjacentHTML("afterend", "<p id='habits_concluded'>Parabens, você concluio todos os seus objetivos!</p>");
+        }
+
+
     }
 }, false);
