66236b881308d8fd8126200609bd0f7c776b8697
Gabriel Hoffman Silva <gabrielhoffman@usp.br>
2020-06-29 10:44:08 -0300

    light mode button

--- js/script.js
+++ js/script.js
@@ -1,20 +1,18 @@
-let theme_button = document.getElementById("theme-switch");
-
 function switch_theme() {
-    const current_theme = document.getElementById("pagestyle")
-    console.log(current_theme.classList);
-    
+	const current_theme = document.getElementById("pagestyle");
 
-    if (current_theme.classList == "dark") {
-        document.getElementById("pagestyle").setAttribute("href", "css/style.css"); 
-        current_theme.classList.remove("dark")
-    }
-    else {
-        document
+	if (current_theme.classList == "dark") {
+		document
+			.getElementById("pagestyle")
+            .setAttribute("href", "css/style.css");
+        console.log("light");
+        
+		current_theme.classList.remove("dark");
+	} else {
+		document
 			.getElementById("pagestyle")
 			.setAttribute("href", "css/dark_mode.css");
+        console.log("dark");
 		current_theme.classList.add("dark");
-    }
+	}
 }
-
-theme_button.addEventListener("click", switch_theme, false);
